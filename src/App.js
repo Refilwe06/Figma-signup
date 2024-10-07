55b26d1d49169cd2d332aea20a3203542cae213c
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-07 23:13:55 +0200

    feat: added basic routes to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Login from './pages/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-    <Route path='/' element={<Login />} exact='true' />
+        <Route path='/' element={<Register />} exact='true' />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
