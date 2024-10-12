5c3267f0b8542660c78be0dd99b114171fa5e8fa
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-13 01:25:01 +0200

    feat: Login logic

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,142 +1,140 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { GoogleLogin } from '@react-oauth/google';
-import './Login.css'; // Import your CSS file for styling
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  const [error, setError] = useState(null);
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        rememberMe: false
     });
-  };
+    const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Send a POST request to the backend for registration
-      const response = await axios.post('http://localhost:5000/register', formData);
-      
-      // Handle successful registration
-      console.log('Registration successful', response.data);
+    // Handle input changes for all form fields
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
+    };
 
-      // Clear form data and error
-      setFormData({ name: '', email: '', password: '' });
-      setError(null);
-      
-      // Optionally redirect the user or show a success message
-      alert('Registration successful! You can now log in.');
-    } catch (err) {
-      // Handle errors during registration
-      const errorMessage = err.response?.data?.err || 'An error occurred';
-      console.error('Error registering user:', errorMessage);
-      setError(errorMessage);
+    // Handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { email, password } = formData;
+        // Input Validations
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordRegex = /^(?=.*\d).{8,}$/;
+
+        if (!email || !password) {
+            setError('Please fill in all fields.');
+            return;
+        }
+        if (!emailRegex.test(email)) {
+            setError('Please enter a valid email address.');
+            return;
+        }
+        if (!passwordRegex.test(password)) {
+            setError('Password must be at least 8 characters long, and include at least one number.');
+            return;
+        }
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, formData);
+            console.log('Registration successful', response.data);
+
+            // Clear form after successful registration
+            setFormData({ email: '', password: '', rememberMe: false });
+            setError(null);
+
+            alert('Registration successful! You can now log in.');
+        } catch (err) {
+            const errorMessage = err?.response?.data?.err || 'An error occurred';
+            console.error('Error registering user:', errorMessage);
+            setError(errorMessage);
+        }
+    };
+
+    const googleAuth = () => {
+        console.log(process.env.REACT_APP_API_URL)
+        window.open(`${process.env.REACT_APP_API_URL}/auth/google/callback`, '_self');
     }
-  };
 
-  const handleGoogleSuccess = async (response) => {
-    const token = response.credential;
-    console.log('Google token:', token);
+    return (
+        <main>
 
-    try {
-      const res = await axios.post('https://signup-backend-intf.onrender.com/auth/google', { token });
-      console.log('Google authentication successful:', res.data);
-      // Handle successful Google authentication
-    } catch (err) {
-      // Handle errors during Google authentication
-      const errorMessage = err.response?.data?.message || 'An error occurred';
-      console.error('Error during Google authentication:', errorMessage);
-      setError(errorMessage);
-    }
-  };
+            <div className="container">
+                <div className="left-content">
+                    <div className="header">
+                        <div className="logo">
+                            <img src={process.env.PUBLIC_URL + '/Logo.png'} alt="Logo" />
+                        </div>
+                    </div>
 
-  const handleGoogleError = (error) => {
-    console.error('Google login error:', error);
-    setError('Google login failed. Please try again.');
-  };
+                    <div className="wrapper">
+                        <div className="sign-up-text">
+                            <h1 className="bebas-neue-regular">
+                                SIGN IN
+                            </h1>
+                            <p>Log in to your account</p>
+                        </div>
 
-  return (
-    <div className="container">
-      <div className='Left-content'>
-        <div className="header">
-          <div className="logo">
-            <img src={process.env.PUBLIC_URL + '/Logo.png'} alt="Logo" />
-          </div>
-        </div>
-        <div className="heading">
-          <img src={process.env.PUBLIC_URL + '/Heading.png'} alt="Heading" />
-        </div>
-        
-        <div className="login-details">
-          <br /><br />
-          <div className="google-button-wrapper">
-            <GoogleLogin
-              onSuccess={handleGoogleSuccess}
-              onError={handleGoogleError}
-              render={(renderProps) => (
-                <img
-                  src={process.env.PUBLIC_URL + '/Glogin.png'}
-                  alt="Google Sign-In"
-                  className="google-button"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                />
-              )}
-            />
-          </div>
-          <br /><br />
-          <img src={process.env.PUBLIC_URL + '/Seperater.png'} alt="Separator" className="separator" />
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                value={formData.name}
-                onChange={handleChange}
-              />
+                        <div className="login-details">
+                            <div className="google-button-wrapper">
+                                <img onClick={googleAuth} src={process.env.PUBLIC_URL + '/GoogleButton.svg'} alt='Google Login Button' />
+                            </div>
+
+                            <div className="separator">
+                                <div className='line' ></div>
+                                <p>Or</p>
+                                <div className='line' ></div>
+                            </div>
+                            {error && <p className="error">{error}</p>}
+
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="remember-me">
+                                    <input
+                                        type="checkbox"
+                                        name="rememberMe"
+                                        checked={formData.rememberMe}
+                                        onChange={handleChange}
+                                    />
+                                    <label>
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button className="register-button" type="submit">Login</button>
+                                <p>Not a user? <Link to="/register">Register</Link></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="right-content">
+                    <div className="speaker-container"></div>
+                </div>
             </div>
-            <div className="form-group">
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label>
-                <input type="checkbox" id="remember-me" />
-                Remember me
-              </label>
-            </div>
-            <button className="register-button" type="submit">Register</button>
-            {error && <p className="error">{error}</p>}
-            <p>Already have an account? <span>Log in</span></p>
-          </form>
-        </div>
-      </div>
-      <div className='Right-content'></div>
-    </div>
-  );
+        </main>
+
+    );
 }
 
 export default Login;
-
