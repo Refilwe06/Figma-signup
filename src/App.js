cf1b97b4c6fca668ad173c4406bb28efa4777fb6
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-14 19:33:25 +0200

    fix: handle alert error displayed evertime

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Profile from './pages/Profile/Profile';
 import axios from 'axios';
+import Cookies from 'js-cookie';
+import ProtectedRoutes from './utilities/ProtectedRoutes';
 
 function App() {
-  const [user, setUser] = useState(null);
-
+  const [errorMessage, setErrorMessage] = useState(null); // State to hold error messages
   const getUser = async () => {
     try {
       const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
@@ -16,27 +17,40 @@ function App() {
         withCredentials: true,
         headers: {
           Accept: 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         }
       });
+
       console.log(response?.data?.msg);
-      setUser(response?.data);
     } catch (err) {
-      alert(err?.data?.msg);
+      console.log(err?.data);
+      setErrorMessage(err?.response?.data?.msg); // Set the error message state
       console.error(err);
     }
   };
 
   useEffect(() => {
-    getUser();
+    if (!Cookies.get('token')) getUser();
   }, []);
+
+  useEffect(() => {
+    const hasShownAlert = localStorage.getItem('hasShownAlert'); // Check if alert has been shown
+
+    if (errorMessage && !hasShownAlert) {
+      alert(errorMessage); // Show the alert if there's an error message and alert hasn't been shown
+      localStorage.setItem('hasShownAlert', 'true'); // Set the flag to prevent showing the alert again
+    }
+  }, [errorMessage]); // Run this effect only when errorMessage changes
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Register />} exact='true' />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
