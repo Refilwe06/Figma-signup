55ba6e8e0aa33ef8ee71ba0297e4508eb51eecea
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-16 21:45:28 +0200

    styles: responsive styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css'
 import { UserProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
